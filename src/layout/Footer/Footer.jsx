643abe6dc47f9507c67ddb59a062f24b800d1ecb
youssef7474 <youssef.mohamed.refai@gmail.com>
2024-02-06 20:37:55 +0200

    edits in footer

--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div style={{marginTop:"50px",height:"200px",backgroundColor:"#F5F4F4",display:"flex",alignItems:"center",justifyContent:"center"}}>
-        All rights are resrved To <span style={{color:"black",fontWeight:"bold"}}> YOUSSEF</span>
+    All rights are reserved To &nbsp;<span style={{color:"black",fontWeight:"bold"}}>YOUSSEF</span>
     </div>
   )
 }
