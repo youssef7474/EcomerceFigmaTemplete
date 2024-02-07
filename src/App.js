492c693058b95f551b867d3989ddbdb7c166224b
youssef7474 <youssef.mohamed.refai@gmail.com>
2024-02-07 11:11:45 +0200

    test git karim

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import SecondLanding from './features/homePage/SecondLanding/SecondLanding'
 import SliderOne from './features/homePage/SliderOne/SliderOne'
 import SliderTwo from './features/homePage/SliderTwo/SliderTwo'
 import Cotton from './features/homePage/Cotton/Cotton'
+import FooterKarim from './layout/Footer/FooterKarim'
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
       <SliderOne/>
       <SliderTwo/>
       <Cotton/>
+      <FooterKarim/>
     </div>
   )
 }
---
+++ src/layout/Footer/FooterKarim.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FooterKarim = () => {
+  return (
+    <div style={{backgroundColor:"red",height:"100px",marginTop:"10px"}}>
+      test footer karim
+    </div>
+  )
+}
+
+export default FooterKarim
